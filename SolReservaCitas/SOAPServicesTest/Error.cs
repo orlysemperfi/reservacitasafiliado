70ab212070d3b39273d961173a06948b564f539f
fernando.llanos1@gmail.com <fernando.llanos1@gmail.com@e4dc2da9-a4d0-f05e-0bf7-be27fac63200>
2014-02-08 03:53:28 +0000

    Clase de Error

---
+++ SolReservaCitas/SOAPServicesTest/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOAPServicesTest
+{
+    public class Error
+    {
+        public string CodigoNegocio { get; set; }
+        public string MensajeNegocio { get; set; }
+    }
+}
